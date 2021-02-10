9e317100905bda5609037106b1b8b895d582e9c7
Jamil Ur Rehman Ahmadzai <jamilurrehman722@gmail.com>
2021-02-10 02:58:51 +0100

    added popup to the map to show the selected region info

--- src/sort.js
+++ src/sort.js
@@ -43,6 +43,23 @@ export const showDataOnMap = (data, casesType = "cases") =>
         Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
       }
     >
-      <Popup>PopUUPPP</Popup>
+      <Popup>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          ></div>
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
+      </Popup>
     </Circle>
   ));
